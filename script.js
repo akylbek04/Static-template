bd8c33263c162fc578e6e1ca8fafc998adcb516f
Akylbek04 <zamirovakylbek920@gmail.com>
2023-07-09 12:26:41 +0200

    add searching functionality

--- script.js
+++ script.js
@@ -9,9 +9,15 @@ const pageStatus = document.querySelector("#page-status");
 const allTheBooks = document.querySelector("#allBooks");
 const favBooks = document.querySelector("#favoriteBooks");
 const archived = document.querySelector("#archivedBooks");
+const searchInput = document.querySelector("#searchInput")
 let allBooks = []
 let activePage = 1;
 let itemsPerPage = 5;
+let filteredData = [];
+
+
+
+
 
 (async () => {
     const res = await axios.get(url);
@@ -42,45 +48,45 @@ const displayAllTheBooks = () => {
 const renderData = (books) => {
     const totalPage = books.length / itemsPerPage
     pageStatus.innerText = activePage + ' / ' + totalPage;
-
+    
     const endIndex = activePage * itemsPerPage;
     const startIndex = (activePage - 1) * itemsPerPage;
     const sliced = books.slice(startIndex, endIndex);
     tBody.innerHTML = "";
     // books = []
     if(sliced.length){
-            sliced.forEach((book, index) => {
+        sliced.forEach((book, index) => {
             const {img, author, title, year, id} = book
             const row = `
             
             <tr>
-                <td>${index + 1}</td>
-                <td>
-                    <img
-                    src="${img}"
-                    width="50"
-                    height="50"
-                    alt=""
-                    />
-                </td>
-                <td>${title}</td>
-                <td>${author.length ? author : 'N/A' }</td>
-                <td>${year}</td>
-                <td class="d-flex align-items-center justify-content-around">
-                    <i onclick="getFav('${id}')" class="fa fa-heart fs-5 text-danger"></i>
-                    <i onclick="getArc('${id}')" class="fa fa-archive fs-5 text-warning"></i>
-                </td>
-                </tr>
+            <td>${index + 1}</td>
+            <td>
+            <img
+            src="${img}"
+            width="50"
+            height="50"
+            alt=""
+            />
+            </td>
+            <td>${title}</td>
+            <td>${author.length ? author : 'N/A' }</td>
+            <td>${year}</td>
+            <td class="d-flex align-items-center justify-content-around">
+            <i onclick="getFav('${id}')" class="fa fa-heart fs-5 text-danger"></i>
+            <i onclick="getArc('${id}')" class="fa fa-archive fs-5 text-warning"></i>
+            </td>
+            </tr>
             `
-           
-
+            
+            
             tBody.innerHTML += row;
-           
+            
         });
     } else {
         tBody.innerHTML = `
         <tr>
-            <td colspan="6" class="text-center">No Data Found</td>
+        <td colspan="6" class="text-center">No Data Found</td>
         </tr>
         `
     }
@@ -103,7 +109,7 @@ nextBtn.addEventListener("click", () => {
     } else {
         activePage++;
     }
-
+    
     renderData(allBooks);
 });
 
@@ -116,6 +122,17 @@ prevBtn.addEventListener("click", () => {
     renderData(allBooks);
 });
 
+const searchBook = () => {
+    const res = []
+    allBooks.forEach(book => {
+        if(book.title.toLowerCase().includes(searchInput.value)){
+            res.push(book)
+        }
+    })
+    renderData(res);
+}
+
+searchInput.addEventListener('keyup', searchBook)
 favBooks.addEventListener("click", displayFavorites);
 archived.addEventListener("click", displayArchived);
 allTheBooks.addEventListener("click", displayAllTheBooks);
